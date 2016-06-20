493d9fc7d284e4318188a1997a1996e2180af5ea
Fabricio Puerto <fabricio.puerto@outlook.com>
2016-06-20 21:42:09 +0200

    Created the database, migrated and seed

---
+++ db/migrate/20160620191502_create_bookings.rb
@@ -0,0 +1,11 @@
+class CreateBookings < ActiveRecord::Migration
+  def change
+    create_table :bookings do |t|
+      t.string :firstname
+      t.string :lastname
+      t.string :email
+      t.string :phone
+      t.timestamps null: false
+    end
+  end
+end
